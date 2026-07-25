44f3ea82892a02294a67d7ca9d1fc64d30a0c980
Vishwa M <vishwam776@gmail.com>
2026-07-25 18:48:11 +0530

    Time: 13 ms (70.92%), Space: 46 MB (97.95%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,26 +1,23 @@
 class Solution {
     public static boolean isvowel(char c){
-        if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')return true;
-        return false;
+        return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+        
     }
     public int maxVowels(String s, int k) {
          int n=s.length();
          int vowelcount=0;
-         int maxi=Integer.MIN_VALUE;
-         int l=0,r=0;
-         while(r<s.length()){
-            if(r<k){
-               if(isvowel(s.charAt(r))) vowelcount++;
-               r++;
-            }
-            else{
-                if(isvowel(s.charAt(l))) vowelcount--;
-                l++;
-                if(isvowel(s.charAt(r))) vowelcount++;
-                r++;
-            }
-            System.out.println(vowelcount +" "+r+" "+l);
+         for(int i=0;i<k;i++){
+               if(isvowel(s.charAt(i))) vowelcount++;
+         }
+         int maxi=vowelcount;
+         if(maxi==k)return k;
+
+         for(int i=k;i<n;i++){
+                if(isvowel(s.charAt(i-k))) vowelcount--;
+                if(isvowel(s.charAt(i))) vowelcount++;
+
             maxi=Math.max(maxi,vowelcount);
+            if(maxi==k)return k;
          }
          return maxi;
     }
