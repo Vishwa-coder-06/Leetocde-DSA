271e4359cb70e5c7af4e92fbcfd54ed7337ca940
Vishwa M <vishwam776@gmail.com>
2026-08-17 21:42:40 +0530

    Time: 2 ms (99.72%), Space: 45.2 MB (24.11%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+     static void choice(int i,int[] nums, ArrayList<Integer>sub, List<List<Integer>>res){
+        //bc
+        if(i==nums.length){
+            res.add(new ArrayList<>(sub));
+            return;
+        }
+        //take it
+        sub.add(nums[i]);
+        choice(i+1,nums, sub, res);
+        // dont take it
+        sub.remove(sub.size()-1);
+        while(i+1<nums.length && nums[i]==nums[i+1]) i++;
+        choice(i+1, nums, sub, res);
+    } 
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res=new ArrayList<>();
+        choice(0, nums, new ArrayList<>(),res);
+        return res;
+    }
+}
