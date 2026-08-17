8e478040d5d141ac9ef20aec86d71a69d2ab2c5e
Vishwa M <vishwam776@gmail.com>
2026-08-17 21:23:45 +0530

    Time: 0 ms (100%), Space: 44.4 MB (73.41%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,20 +1,26 @@
 class Solution {
-    static void gen(int open,int close,int n,String paren,ArrayList<String> res){
+    static void gen(int open,int close,int n,StringBuilder paren,ArrayList<String> res){
     //bc 
-    if(open == n && close == n) {
-        res.add(paren);
+    if(paren.length() == n*2) {
+        res.add(paren.toString());
         return ;
     }
     //call
-    if(open<n)
-    gen(open+1,close,n,paren+"(",res);
-    if(open>close)
-    gen(open,close+1,n,paren+")",res);
+    if(open<n){
+        paren.append("(");
+        gen(open+1,close,n,paren,res);
+        paren.deleteCharAt(paren.length()-1);
+    }
+    if(open>close){
+        paren.append(")");
+        gen(open,close+1,n,paren,res);
+        paren.deleteCharAt(paren.length()-1);
    }
+}
 
     public List<String> generateParenthesis(int n) {
         ArrayList<String> res=new ArrayList<>();
-        gen(0,0,n,"",res);
+        gen(0,0,n,new StringBuilder(),res);
         return res;
     }
 }
