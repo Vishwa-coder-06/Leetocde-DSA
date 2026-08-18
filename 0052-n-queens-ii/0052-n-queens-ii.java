823dbc808a2f4c8bc95c784cf5d977870115c05e
Vishwa M <vishwam776@gmail.com>
2026-08-18 21:23:41 +0530

    Time: 1 ms (95.92%), Space: 42.3 MB (26.31%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,28 +1,31 @@
 class Solution {
      static int backtrack(int col, int n, 
-                         HashSet<Integer>rowSet,
-                         HashSet<Integer>ldSet,
-                         HashSet<Integer>rdSet){
+                        boolean[] rows,boolean[] ldiag,boolean[] rdiag){
        if(col==n){
             return 1;
        }
        int count=0;
        for(int row=0;row<n;row++){
-        if(!(rowSet.contains(row) || 
-             ldSet.contains(row+col) ||
-             rdSet.contains(row-col)  )){
-                rowSet.add(row);
-                ldSet.add(row+col);
-                rdSet.add(row-col);
-                count+=backtrack(col+1,n,rowSet,ldSet,rdSet);
-                rowSet.remove(row);
-                ldSet.remove(row+col);
-                rdSet.remove(row-col);
-            }
+        int lInd=row+col;
+        int rInd =row-col + n-1;
+
+        if(!(rows[row] || ldiag[lInd] || rdiag[rInd])){
+        rows[row]=true;
+        ldiag[lInd]=true;
+        rdiag[rInd]=true;
+
+        count+=backtrack(col+1,n,rows,ldiag,rdiag);
+        rows[row]=false;
+        ldiag[lInd]=false;
+        rdiag[rInd]=false;
+         }
        }
        return count;
     }
     public int totalNQueens(int n) {
-       return backtrack(0,n,new HashSet<>(),new HashSet<>(),new HashSet<>());
+        boolean[] rows=new boolean[n];
+        boolean[] ldiag=new boolean[2*n];
+        boolean[] rdiag=new boolean[2*n];
+       return backtrack(0,n,rows,ldiag,rdiag);
     }
 }
