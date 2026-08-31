e9df3b000aea80c9b90436354cfdd1348be4c830
Vishwa M <vishwam776@gmail.com>
2026-08-31 21:49:19 +0530

    Time: 1 ms (99.83%), Space: 47.8 MB (38.45%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,34 @@
+class Solution {
+    void dfs(int r,int c,char[][] board){
+        if(r<0 || r>=board.length || c<0 || c>=board[0].length || board[r][c] != 'O')
+        return;
+        board[r][c] ='B';
+        dfs(r-1,c,board);
+        dfs(r+1,c,board);
+        dfs(r,c-1,board);
+        dfs(r,c+1,board);
+    }
+    public void solve(char[][] board) {
+        if(board == null || board.length == 0)return;
+        int n=board.length;
+        int m=board[0].length;
+        //first and last row
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O') dfs(i,0,board);
+            if(board[i][m-1]=='O') dfs(i,m-1,board);
+        }
+        //first and last col
+        for(int i=0;i<m;i++){
+            if(board[0][i]=='O') dfs(0,i,board);
+            if(board[n-1][i]=='O') dfs(n-1,i,board);
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='B') board[i][j]='O';
+            }
+        }
+        
+    }
+}
