4c2ac585bb2d8f2f4409333f48fd6df97fb9d706
Vishwa M <vishwam776@gmail.com>
2026-09-11 22:08:54 +0530

    Time: 9 ms (87.48%), Space: 46.5 MB (44.44%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int swimInWater(int[][] grid) {
+        int n=grid.length;
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        boolean[][] visited=new boolean[n][n];
+
+        pq.offer(new int[]{grid[0][0], 0, 0});
+        visited[0][0]=true;
+        int[][] direction={{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!pq.isEmpty()){
+            int[] curr=pq.poll();
+            int time=curr[0];
+            int r=curr[1];
+            int c=curr[2];
+            if(r==n-1 && c==n-1) return time;
+
+            for(int[] dir:direction){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+                if(nr>=0 && nr<n && nc>=0 && nc<n && !visited[nr][nc]){
+                    visited[nr][nc]=true;
+                    pq.offer(new int[]{Math.max(time,grid[nr][nc]),nr,nc});
+                }
+            }
+        }
+        return 0;
+    }
+}
