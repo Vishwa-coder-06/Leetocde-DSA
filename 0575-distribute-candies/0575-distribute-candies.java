271e1c1b09a6268c2f21bfa6470d382869beeb15
Vishwa M <vishwam776@gmail.com>
2026-09-25 22:37:41 +0530

    Time: 30 ms (91.21%), Space: 48.5 MB (68.33%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int candy:candyType) set.add(candy);
+
+        return Math.min(candyType.length/2,set.size());  
+    }
+}
